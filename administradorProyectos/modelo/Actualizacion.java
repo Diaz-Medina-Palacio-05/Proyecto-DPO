bbc7e0a61cf9da0810aa412e6c72a29b8940bb52
NCKDayz <88889627+NCKDayz@users.noreply.github.com>
2022-03-25 23:26:10 -0500

    Add files via upload
    Este si o si es el esqueleto final perdon

---
+++ administradorProyectos/modelo/Actualizacion.java
@@ -0,0 +1,21 @@
+package administradorProyectos.modelo;
+
+import java.io.File;
+
+public class Actualizacion 
+{
+	//Atributos
+	private File archivoBase;
+	
+	//Constructor
+	public Actualizacion(File archivoBase)
+	{
+		this.archivoBase = archivoBase;
+	}
+	
+	//Metodos
+	public void actualizarArchivo()
+	{
+		//TODO
+	}
+}
